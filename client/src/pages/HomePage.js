908b6b6ccbf10b02ba8d2fa6a6721c3f8bc8427d
unknown <apsharma202003@gmail.com>
2023-10-22 14:16:17 +0530

    recent

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,13 @@
-import React,{useEffect} from 'react'
+import React,{ useEffect } from 'react'
 import axios from 'axios'
 import { Layout } from 'antd'
-const { Content } = Layout;
+
+
 const HomePage = () => {
   // login user data 
   const getUserData = async () =>{
 try{
+  
      const res=await axios.post('/api/v1/user/getUserData',{},{
   headers:{
     Authorization : "Bearer " + localStorage.getItem('token'),
@@ -23,7 +25,10 @@ catch(error){
   return (
 
     <Layout>
-           <h1>Home Page</h1>  
+     
+      <h1>Home Page</h1>
+   
+             
     </Layout>
   )
 }
