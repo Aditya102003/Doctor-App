23d6513d59e56d84def144cdeaf1149d9fd00aeb
unknown <apsharma202003@gmail.com>
2023-10-29 00:19:39 +0530

    commit

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.get("/", (req, res) => {
 });
 
 // static files 
-app.use(express.static(path.join(__dirname,"./client/build")));
+app.use(express.static(path.join(__dirname,"./client/public")));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+    res.sendFile(path.join(__dirname,"./client/public/index.html"));
   });
 
 // // port 
